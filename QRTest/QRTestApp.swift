64f90a16568e352de4baed86ed3cfd05c9d20346
Jhonny Montoya <jhonnymontoya@gmail.com>
2024-10-12 18:16:03 -0400

    Initial Commit

---
+++ QRTest/QRTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRTestApp.swift
+//  QRTest
+//
+//  Created by Jhonny Montoya on 2024-10-12.
+//
+
+import SwiftUI
+
+@main
+struct QRTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
