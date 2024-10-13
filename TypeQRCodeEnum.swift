a884fa7779c4c382df3e49cba23966bd8a221b71
Jhonny Montoya <jhonnymontoya@gmail.com>
2024-10-12 21:53:08 -0400

    Add Enum to represent types of QR Codes

---
+++ TypeQRCodeEnum.swift
@@ -0,0 +1,40 @@
+//
+//  TypeQRCodeEnum.swift
+//  QRTest
+//
+//  Created by Jhonny Montoya on 2024-10-12.
+//
+
+///Enum for type of QR Codes
+enum TypeQRCodeEnum {
+    
+    ///WebSite
+    case website
+    
+    ///SMS
+    case sms
+    
+    ///vCard version 3
+    case vcard
+    
+    ///Social Media
+    case socialmedia
+    
+    ///WhatsApp messages
+    case whatsapp
+    
+    ///Calendar iCal / vEvent
+    case calendar
+    
+    ///GeoLocalization points
+    case geolocalization
+    
+    ///One Time Password (OTP)
+    case otp
+    
+    ///Phone calls
+    case phone
+    
+    ///WiFi Passwords
+    case wifi
+}
